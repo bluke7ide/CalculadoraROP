7b32bde03c57dc61451d26ae185f7133ba8d6220
bluke7ide <luisfernandoamey@gmail.com>
2025-06-18 10:45:47 -0600

    feat: intereses

--- cod/intereses.R
+++ cod/intereses.R
@@ -1,6 +1,8 @@
-intereses <- function(rendimientos, monto){
-  int <- rendimientos/monto
-  mean_int <- mean(int)
-  std_int <- sd(int)
-  
+intereses <- function(x){
+  set.seed(2025)
+  tasas <- sapply(x:115, function(x)
+  ifelse(runif(1) < 0.05,
+         qnorm(runif(1), mean = -0.05, sd = 0.01),
+         qnorm(runif(1), mean = 0.09, sd = 0.03)))
+  return(tasas)
 }
