69d897e5d17945d6947295d625596d422d0917dd
bluke7ide <luisfernandoamey@gmail.com>
2025-06-18 12:46:05 -0600

    feat: grafico renta per

--- cod/intereses.R
+++ cod/intereses.R
@@ -1,5 +1,4 @@
 intereses <- function(x){
-  set.seed(2025)
   tasas <- sapply(x:115, function(x)
   ifelse(runif(1) < 0.05,
          qnorm(runif(1), mean = -0.05, sd = 0.01),
--- cod/renta_permanente.R
+++ cod/renta_permanente.R
@@ -1,17 +1,17 @@
 renta_permanente <- function(rendimientos, x, monto, año_jub, vec_rend){ # no se ocupa el sexo no hay mortalidad.
-  renta_per <- sum(rendimientos)/36
-  minpen <- 30638.35
-  res <- data.frame(res_inicial = 0, edad = x+0:(115-x), pension = 0, tasa = vec_rend, rend = 0, res_final = 0)
+  res <- data.frame(edad = x+0:(115-x),res_inicial = 0, pension = 0, 
+                    tasa = vec_rend, rend = 0, res_final = 0)
   res$res_inicial[1] <- monto
-  res$pension[1:3] <- max(renta_per, minpen)
-  res$rend[1] <- (monto-6*res$pension[1])**res$tasa[1]
+  res$pension[1:3] <- max(sum(rendimientos)/36, 30638.35)
+  res$rend[1] <- (monto-6*res$pension[1])*res$tasa[1]
   res$res_final[1] <- monto + res$rend[1] - 12*res$pension[1]
   for(i in 2:(115-x+1)){
     if((i-1)%%3 == 0){
-      
+      res$pension[1:3+(i-1)] <- max(sum(res$rend[1:3+(i-4)])/36, 30638.35)
     }
+    res$res_inicial[i] <- res$res_final[i-1]
+    res$rend[i] <- (res$res_inicial[i]-6*res$pension[i])*res$tasa[i]
+    res$res_final[i] <- res$res_inicial[i] + res$rend[i] - 12*res$pension[i]
   }
   return(res)
 }
-
-renta_permanente(rendimientos, 65, 100, 0, 1.07)
