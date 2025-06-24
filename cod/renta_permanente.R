41b2142d525b3b32b9331ae9770405cec41dfb79
bluke7ide <luisfernandoamey@gmail.com>
2025-06-23 22:17:52 -0600

    fix: monto renta per

--- cod/renta_permanente.R
+++ cod/renta_permanente.R
@@ -2,7 +2,7 @@ renta_permanente <- function(rendimientos, x, monto, año_jub, vec_rend){ # no s
   res <- data.frame(Edad = x+0:(115-x),res_inicial = 0, Pension_Mensual = 0, 
                     tasa = vec_rend, Rendimientos = 0, Reserva_Final = 0)
   res$res_inicial[1] <- monto
-  res$Pension_Mensual[1:3] <- max(sum(rendimientos)/36, 30638.35)
+  res$Pension_Mensual[1:3] <- max(sum(rendimientos)/36, 31938.44)
   res$Rendimientos[1] <- (monto-6*res$Pension_Mensual[1])*res$tasa[1]
   if(res$res_inicial[1]-12*res$Pension_Mensual[1] <0){
     res$Pension_Mensual[1] <- res$res_inicial[1]/12
@@ -11,9 +11,9 @@ renta_permanente <- function(rendimientos, x, monto, año_jub, vec_rend){ # no s
   for(i in 2:(115-x+1)){
     if((i-1)%%3 == 0){
       if(115-x+1-i <3){
-       res$Pension_Mensual[i:(115-x+1)] <- max(sum(res$Rendimientos[1:3+(i-4)])/36, 30638.35)
+       res$Pension_Mensual[i:(115-x+1)] <- max(sum(res$Rendimientos[1:3+(i-4)])/36, 31938.44)
       } else {
-        res$Pension_Mensual[1:3+(i-1)] <- max(sum(res$Rendimientos[1:3+(i-4)])/36, 30638.35)
+        res$Pension_Mensual[1:3+(i-1)] <- max(sum(res$Rendimientos[1:3+(i-4)])/36, 31938.44)
       }
     }
 
